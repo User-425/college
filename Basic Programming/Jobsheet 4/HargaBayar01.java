8be585d3fe310bbee54c06134b9d57ebb571e401
User-425 <user425.itsme@gmail.com>
2023-09-18 09:47:10 +0700

    Variables declaration

--- Basic Programming/Jobsheet 4/HargaBayar01.java
+++ Basic Programming/Jobsheet 4/HargaBayar01.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class HargaBayar01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int harga, jumlah;
+        double dis=0.1, total, bayar, jmlDis;
     }
 }
