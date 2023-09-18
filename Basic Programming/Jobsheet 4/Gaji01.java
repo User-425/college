b38b64973dd5da2a8a5c7b98aeb6f2d6b1c5f2b9
User-425 <user425.itsme@gmail.com>
2023-09-18 09:30:51 +0700

    Variables declaration

--- Basic Programming/Jobsheet 4/Gaji01.java
+++ Basic Programming/Jobsheet 4/Gaji01.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Gaji01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int jmlMasuk, jmlTdkMasuk, totGaji;
+        int gaji = 40000, potGaji = 25000;
     }
 }
