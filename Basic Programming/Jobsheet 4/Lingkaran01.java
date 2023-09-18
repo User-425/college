349151a13f1d5cf95bbd605f120be3efc134e606
User-425 <user425.itsme@gmail.com>
2023-09-18 09:10:46 +0700

    Variable declaration

--- Basic Programming/Jobsheet 4/Lingkaran01.java
+++ Basic Programming/Jobsheet 4/Lingkaran01.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Lingkaran01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int r;
+        double keliling, luas;
     }
 }
