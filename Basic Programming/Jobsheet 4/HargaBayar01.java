1c399936c31c479d15dbd38218d576296bff6b27
User-425 <user425.itsme@gmail.com>
2023-09-18 09:48:22 +0700

    Code to get input

--- Basic Programming/Jobsheet 4/HargaBayar01.java
+++ Basic Programming/Jobsheet 4/HargaBayar01.java
@@ -5,5 +5,9 @@ public class HargaBayar01 {
         Scanner input = new Scanner(System.in);
         int harga, jumlah;
         double dis=0.1, total, bayar, jmlDis;
+        System.out.println("Masukkan harga barang yang dibeli ");
+        harga = input.nextInt();
+        System.out.println("Masukkan jumlah barang yang dibeli ");
+        jumlah = input.nextInt();
     }
 }
