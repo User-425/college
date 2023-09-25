eac12ade750b4d8fb5368ae730f334e1337067ec
User-425 <user425.itsme@gmail.com>
2023-09-25 12:34:48 +0700

    Input code

--- Basic Programming/Jobsheet 5/PemilihanPercobaan101.java
+++ Basic Programming/Jobsheet 5/PemilihanPercobaan101.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class PemilihanPercobaan101 {
 public static void main(String[] args) {
     Scanner input01 = new Scanner(System.in);
-
+    System.out.println("Masukkan angka: ");
+    int angka = input01.nextInt();
 }
     
 }
