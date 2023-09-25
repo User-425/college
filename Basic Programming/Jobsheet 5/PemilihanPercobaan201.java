5918dd421a00428a544c43be0c163b6f40db6bc9
User-425 <user425.itsme@gmail.com>
2023-09-25 13:12:03 +0700

    added message

--- Basic Programming/Jobsheet 5/PemilihanPercobaan201.java
+++ Basic Programming/Jobsheet 5/PemilihanPercobaan201.java
@@ -11,7 +11,10 @@ public class PemilihanPercobaan201 {
         float kuis = input01.nextFloat();
         System.out.print("Nilai tugas : ");
         float tugas = input01.nextFloat();
-        
+
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F); 
+
+        String message = total < 65 ? "Remidi" : "Tidak remidi"; 
+
     }
 }
