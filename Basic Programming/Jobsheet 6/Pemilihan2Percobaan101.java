dd55631affb35c347b4ff6245c7630a89f637f62
User-425 <user425.itsme@gmail.com>
2023-10-05 12:19:40 +0700

    Main Code

--- Basic Programming/Jobsheet 6/Pemilihan2Percobaan101.java
+++ Basic Programming/Jobsheet 6/Pemilihan2Percobaan101.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Pemilihan2Percobaan101 {
  public static void main(String[] args) {
+    int tahun;
     Scanner input01 = new Scanner(System.in);
+    tahun = input01.nextInt();
+    if ((tahun % 4) == 0) { 
+        if ((tahun % 100) != 0) 
+       System.out.println("Tahun Kabisat");
+     } else 
+     System.out.println("Bukan Tahun Kabisat"); 
  }   
 }
