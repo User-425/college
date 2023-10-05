b7f152f3fe973079f0f1bd7c58edc91c6b08aa17
User-425 <user425.itsme@gmail.com>
2023-10-05 12:13:07 +0700

    Nilai

--- Basic Programming/Jobsheet 5/PemilihanPercobaan201.java
+++ Basic Programming/Jobsheet 5/PemilihanPercobaan201.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan201 {
         public static void main(String[] args) {
+	    String nilai;
         Scanner input01 = new Scanner(System.in);
         System.out.print("Nilai uas : ");
         float uas = input01.nextFloat();
@@ -13,9 +14,23 @@ public class PemilihanPercobaan201 {
         float tugas = input01.nextFloat();
 
         float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F); 
+	String message = total < 65 ? "Remidi" : "Tidak remidi"; 
+       	if (total > 80) {
+            nilai = "A";
+        } else if (total > 73) {
+            nilai = "B+";
+        } else if (total > 65) {
+            nilai = "B";
+        } else if (total > 60) {
+            nilai = "C+";
+        } else if (total > 50) {
+            nilai = "C";
+        } else if (total > 39) {
+            nilai = "D";
+        } else {
+            nilai = "E";
+        }
 
-        String message = total < 65 ? "Remidi" : "Tidak remidi"; 
-
-        System.out.println("Nilai akhir = "+ total + " sehingga " + message);
+        System.out.println("Nilai akhir = "+ total + " sehingga	anda "+ message +" dan mendapat nilai "+ nilai);
     }
 }
