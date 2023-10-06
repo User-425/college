731999ce0147a74a6ded81fa0805489092e9c547
User-425 <user425.itsme@gmail.com>
2023-10-06 07:21:24 +0700

    Modified

--- Basic Programming/Jobsheet 6/Pemilihan2Percobaan101.java
+++ Basic Programming/Jobsheet 6/Pemilihan2Percobaan101.java
@@ -4,10 +4,14 @@ public class Pemilihan2Percobaan101 {
  public static void main(String[] args) {
     int tahun;
     Scanner input01 = new Scanner(System.in);
+    System.out.println("Masukkan Tahun:");
     tahun = input01.nextInt();
     if ((tahun % 4) == 0) { 
-        if ((tahun % 100) != 0) 
-       System.out.println("Tahun Kabisat");
+       if ((tahun % 100) != 0)  {
+        System.out.println("Tahun Kabisat");
+       } else {
+        System.out.println("Bukan Tahun Kabisat"); 
+       }
      } else 
      System.out.println("Bukan Tahun Kabisat"); 
  }   
