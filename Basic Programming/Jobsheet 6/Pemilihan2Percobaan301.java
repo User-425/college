183bc626c9422286b569949fe952c88b3eddda8d
User-425 <user425.itsme@gmail.com>
2023-10-06 07:42:27 +0700

    Scanner Declaration

--- Basic Programming/Jobsheet 6/Pemilihan2Percobaan301.java
+++ Basic Programming/Jobsheet 6/Pemilihan2Percobaan301.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Pemilihan2Percobaan301 {
     public static void main(String[] args) {
-        
+        Scanner input01 = new Scanner(System.in);
     }
 }
