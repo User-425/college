428c1d3457c014ecba7655d1b553c90379214c6b
User-425 <user425.itsme@gmail.com>
2023-10-06 07:31:03 +0700

    Input

--- Basic Programming/Jobsheet 6/Pemilihan2Percobaan201.java
+++ Basic Programming/Jobsheet 6/Pemilihan2Percobaan201.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 
 public class Pemilihan2Percobaan201 {
+    public static void main(String[] args) {
+    int sudut1,sudut2,sudut3;
     Scanner input01 = new Scanner(System.in);
+    System.out.println("Masukkan Sudut1 :");
+    sudut1 = input01.nextInt();
+    System.out.println("Masukkan Sudut2 :");
+    sudut2 = input01.nextInt();
+    System.out.println("Masukkan Sudut3 :");
+    sudut3 = input01.nextInt();
+    }   
 }
