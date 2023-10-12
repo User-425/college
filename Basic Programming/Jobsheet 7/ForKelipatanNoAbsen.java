df9c9739ddc58bf0603978d6abdcc43985784aa7
User-425 <user425.itsme@gmail.com>
2023-10-12 19:44:01 +0700

    Input

--- Basic Programming/Jobsheet 7/ForKelipatanNoAbsen.java
+++ Basic Programming/Jobsheet 7/ForKelipatanNoAbsen.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class ForKelipatanNoAbsen {
     public static void main(String[] args) {
         int kelipatan, jumlah=0, counter=0;
-        Scanner input01 = new Scanner(System.in);
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
     }
 }
