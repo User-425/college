0099fc7293fcc64ae1851e2631327596df785c44
User-425 <user425.itsme@gmail.com>
2023-10-12 19:43:01 +0700

    initialize variable

--- Basic Programming/Jobsheet 7/ForKelipatanNoAbsen.java
+++ Basic Programming/Jobsheet 7/ForKelipatanNoAbsen.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
 public class ForKelipatanNoAbsen {
     public static void main(String[] args) {
+        int kelipatan, jumlah=0, counter=0;
+        Scanner input01 = new Scanner(System.in);
         
     }
 }
