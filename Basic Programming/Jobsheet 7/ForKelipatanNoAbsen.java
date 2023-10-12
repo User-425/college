1365877f753b469d576df38d607fc26a87130cf6
User-425 <user425.itsme@gmail.com>
2023-10-12 19:47:42 +0700

    Output Code

--- Basic Programming/Jobsheet 7/ForKelipatanNoAbsen.java
+++ Basic Programming/Jobsheet 7/ForKelipatanNoAbsen.java
@@ -5,5 +5,13 @@ public class ForKelipatanNoAbsen {
         Scanner scan = new Scanner(System.in);
         System.out.println("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
+        for (int i = 1; i <= 50; i++) {
+            if (i%kelipatan==0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        System.out.println("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n",kelipatan,counter);
+        System.out.println("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n",kelipatan,jumlah);
     }
 }
