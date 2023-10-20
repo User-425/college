a744c838cb9250750bc70e67ae82277d5520eb16
User-425 <user425.itsme@gmail.com>
2023-10-20 09:16:47 +0700

    Variable Declaration

--- Basic Programming/Jobsheet 7/WhileGaji01.java
+++ Basic Programming/Jobsheet 7/WhileGaji01.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class WhileGaji01 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
     }
 }
