80b9c7ba2c17fdac370ec88e8bc4ccc4bb089da7
User-425 <user425.itsme@gmail.com>
2023-10-20 09:28:40 +0700

    Main Code

--- Basic Programming/Jobsheet 7/WhileGaji01.java
+++ Basic Programming/Jobsheet 7/WhileGaji01.java
@@ -8,5 +8,23 @@ public class WhileGaji01 {
         String jabatan;
         System.out.println("Masukkan Jumlah Karyawan: ");
         jumlahKaryawan = scan.nextInt();
+        int i = 0;
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke-" + (i + 1) + ": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+            if (jabatan.equalsIgnoreCase("direktur")) {
+                gajiLembur = 0;
+            } else if(jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = jumlahJamLembur * 100000;
+            } else if (jabatan.equalsIgnoreCase("karyawan")){
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+            totalGajiLembur += gajiLembur;
+        }
+        System.out.println("Total gaji lembur: " + totalGajiLembur);
     }
 }
