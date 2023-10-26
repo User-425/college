7b8da5c1080cbb95d8eafdff502cf0424d5839a0
User-425 <user425.itsme@gmail.com>
2023-10-27 01:07:47 +0700

    Setting up data

--- Basic Programming/Jobsheet 9/ArrayBilangan01.java
+++ Basic Programming/Jobsheet 9/ArrayBilangan01.java
@@ -1,5 +1,10 @@
 public class ArrayBilangan01 {
     public static void main(String[] args) {
         int[] bil = new int[4];
+
+        bil[0] = 5;
+        bil[1] = 13;
+        bil[2] = -7;
+        bil[3] = 17;
     }
 }
