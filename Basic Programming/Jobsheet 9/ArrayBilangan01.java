721c54c2b04a3a47743d9b64ab005259e74b88d3
User-425 <user425.itsme@gmail.com>
2023-10-27 01:08:15 +0700

    output array

--- Basic Programming/Jobsheet 9/ArrayBilangan01.java
+++ Basic Programming/Jobsheet 9/ArrayBilangan01.java
@@ -6,5 +6,10 @@ public class ArrayBilangan01 {
         bil[1] = 13;
         bil[2] = -7;
         bil[3] = 17;
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
     }
 }
