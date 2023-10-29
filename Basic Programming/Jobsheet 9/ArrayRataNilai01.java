54c1f1e1dd802b80d90eeaf051c8bafaf680f66f
User-425 <user425.itsme@gmail.com>
2023-10-29 09:53:39 +0700

    Input colleger number

--- Basic Programming/Jobsheet 9/ArrayRataNilai01.java
+++ Basic Programming/Jobsheet 9/ArrayRataNilai01.java
@@ -4,10 +4,13 @@ public class ArrayRataNilai01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int[] nilaiMhs = new int[10];
-        int jumlahLulus = 0;
+        int jumlahLulus = 0, jumlahMhs;
         double total = 0;
         double rata2;
+
+        System.out.println("Masukkan jumlah mahasiswa");
+        jumlahMhs = input.nextInt();
+        int[] nilaiMhs = new int[jumlahMhs];
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
