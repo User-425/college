dfe868b731d4c394cf40f3bd4c870db57e903453
User-425 <user425.itsme@gmail.com>
2023-10-29 09:47:37 +0700

    looping to sum total

--- Basic Programming/Jobsheet 9/ArrayRataNilai01.java
+++ Basic Programming/Jobsheet 9/ArrayRataNilai01.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 public class ArrayRataNilai01 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.println("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            nilaiMhs[i]=input.nextInt();
+        }
+
+        for (int i = 0; i < nilaiMhs.length; i++) {
+         total += nilaiMhs[i];   
+        }
+
+        
     }
 }
