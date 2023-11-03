84627e42cec0bc388d2e95217abdf272995d0783
User-425 <user425.itsme@gmail.com>
2023-11-03 09:52:42 +0700

    Output

--- Basic Programming/Jobsheet 10/Numbers01.java
+++ Basic Programming/Jobsheet 10/Numbers01.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Numbers01 {
@@ -9,6 +8,9 @@ public class Numbers01 {
         myNumbers[2]= new int[1];
         for (int i = 0; i < myNumbers.length; i++) {
             System.out.println(Arrays.toString(myNumbers[i]));
+        }
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
         }
     }
 }
