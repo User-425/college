13b31e05cc97b9734c7171dd118d3853a93397cf
User-425 <user425.itsme@gmail.com>
2023-11-03 08:39:35 +0700

    Error Handling

--- Basic Programming/Jobsheet 10/BioskopWithScanner01.java
+++ Basic Programming/Jobsheet 10/BioskopWithScanner01.java
@@ -24,12 +24,35 @@ public class BioskopWithScanner01 {
                 System.out.println("Masukkan kolom: ");
                 kolom = sc.nextInt();
                 sc.nextLine();
-                penonton[baris - 1][kolom - 1] = nama;
+               if (baris >= 1 && baris <= penonton.length && kolom >= 1 && kolom <= penonton[0].length) {
+                    if (penonton[baris - 1][kolom - 1] == null) {
+                        penonton[baris - 1][kolom - 1] = nama;
+                        System.out.println("Data penonton berhasil dimasukkan!");
+                    } else {
+                        while (penonton[baris - 1][kolom - 1] != null) {
+                        System.out.println("Kursi sudah ditempati. Silakan pilih kursi lain.");
+                        System.out.println("Masukkan baris: ");
+                        baris = sc.nextInt();
+                        System.out.println("Masukkan kolom: ");
+                        kolom = sc.nextInt();
+                        }
+                    }
+                } else {
+                    System.out.println("Nomor baris/kolom kursi tidak valid!");
+                }
             } else if (next.equals("2")) {
                 System.out.println("Daftar Penonton:");
                 for (int i = 0; i < penonton.length; i++) {
-                    System.out.print(penonton[i][0] + " ");
-                    System.out.print(penonton[i][1]);
+                    if (penonton[i][0] != null) {
+                        System.out.print(penonton[i][0] + "\t");
+                    } else {
+                        System.out.print("***\t|\t");
+                    }
+                    if (penonton[i][1] != null) {
+                        System.out.print(penonton[i][1]);
+                    } else {
+                        System.out.print("***");
+                    }
                     System.out.println();
                 }
             } else if (next.equals("3")) {
