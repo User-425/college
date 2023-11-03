55ef158382afa4b457f1565e211d6834f2f48ee4
User-425 <user425.itsme@gmail.com>
2023-11-03 08:09:39 +0700

    variable declarations

--- Basic Programming/Jobsheet 10/BioskopWithScanner01.java
+++ Basic Programming/Jobsheet 10/BioskopWithScanner01.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class BioskopWithScanner01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int baris, kolom;
+        String nama, next;
+
+        
     }
 }
