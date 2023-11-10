d0a5fb865b9b16a45251a25e6c5b10bf8a3a998a
User-425 <user425.itsme@gmail.com>
2023-11-10 08:12:28 +0700

    added average temperature

--- Basic Programming/Jobsheet 11/NestedLoop_2341720105.java
+++ Basic Programming/Jobsheet 11/NestedLoop_2341720105.java
@@ -6,7 +6,7 @@ public class NestedLoop_2341720105 {
         Double[][] array = new Double[5][7];
 
         for (int i = 0; i < array.length; i++) {
-            System.out.println("Kota ke-" + i);
+            System.out.println("Kota ke-" + i+1);
             for (int j = 0; j < array[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 array[i][j] = input.nextDouble();
@@ -16,10 +16,14 @@ public class NestedLoop_2341720105 {
         
         int i = 1; 
         for (Double[] row : array) {
+            double totalSuhu = 0;
             System.out.print("Kota ke-" + i++ + ": ");
             for (Double element : row) {
                 System.out.print(element + " ");
+                totalSuhu += element;
             }
+            System.out.println();
+            System.out.printf("Rata-rata suhu kota ke-%d: %.2f", i-1 , totalSuhu / row.length);
             System.out.println();
         }
     }
