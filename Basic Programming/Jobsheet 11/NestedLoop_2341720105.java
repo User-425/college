baefcf1298d08793d83f093c05ead1bcc5dc3680
User-425 <user425.itsme@gmail.com>
2023-11-10 07:55:27 +0700

    main code

--- Basic Programming/Jobsheet 11/NestedLoop_2341720105.java
+++ Basic Programming/Jobsheet 11/NestedLoop_2341720105.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
+
 public class NestedLoop_2341720105 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        Double[][] array = new Double[5][7];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Kota ke-" + i);
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print("Hari ke-" + (j + 1) + ": ");
+                array[i][j] = input.nextDouble();
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
