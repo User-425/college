1924841c55ba4660dbef617600c9991ab6606184
User-425 <user425.itsme@gmail.com>
2023-11-10 07:26:20 +0700

    class import and declarations

--- Basic Programming/Jobsheet 11/Quiz.java
+++ Basic Programming/Jobsheet 11/Quiz.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Quiz {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
     }
 }
