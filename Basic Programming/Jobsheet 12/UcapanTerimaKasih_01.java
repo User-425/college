8613bd3b5b8149de8bb95c634bedd045430e95eb
User-425 <user425.itsme@gmail.com>
2023-11-24 07:49:41 +0700

    added ucapanTambahan

--- Basic Programming/Jobsheet 12/UcapanTerimaKasih_01.java
+++ Basic Programming/Jobsheet 12/UcapanTerimaKasih_01.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
 
 public class UcapanTerimaKasih_01 {
-    public static String PenerimaUcapan(){
+
+    public static String ucapan ;
+
+    public static String PenerimaUcapan() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan Nama orang yang ingin anda beri ucapan: ");
         String namaOrang = sc.nextLine();
-        sc.close();
+        System.out.println("Tuliskan Ucapan tambahan yang ingin diberikan: ");
+        ucapan = sc.nextLine();
         return namaOrang;
     }
+
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
+
     public static void ucapanTerimaKasih() {
-       String nama = PenerimaUcapan();
-       System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+"You inspired in me a love for learning and made me feel like I could ask you anything"); 
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you " + nama + " for being the best teacher in the world.\n"
+                + "You inspired in me a love for learning and made me feel like I could ask you anything");
     }
 
     public static void main(String[] args) {
         ucapanTerimaKasih();
+        UcapanTambahan(ucapan);
     }
 }
